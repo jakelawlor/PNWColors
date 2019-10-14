9af2e66d6085ccafd19d149b16ae900178bc9203
Jake Lawlor <jakealawlor@gmail.com>
2019-10-14 03:24:02 -0700

    edited type assumptions
    assumed "continuous" if n>length, and "discrete" if n<length

--- R/WAcolors.R
+++ R/WAcolors.R
@@ -20,7 +20,7 @@
 pnw_palettes <- list(
   Starfish = rbind(c('#24492e', '#015b58', '#2c6184', '#59629b', '#89689d', '#ba7999', '#e69b99'),c(7,4,5,3,1,6,2)),
   Baker = rbind(c('#33271e', '#74677e', '#ac8eab', '#d7b1c5', '#ebbdc8', '#f2cec7', '#f8e3d1', '#fefbe9'),c(2,7,4,6,1,8,5,3)),
-  Bay = rbind(c('#00496f', '#0f85a0', '#edd746', '#ed8b00', '#dd4124'),c(4,1,2,5,3)),
+  Bay = rbind(c('#00496f', '#0f85a0', '#edd746', '#ed8b00', '#dd4124'),c(4,1,3,5,2)),
   Winter = rbind(c('#2d2926', '#33454e', '#537380', '#81a9ad', '#ececec'),c(1,4,5,2,3)),
   Lake = rbind(c('#362904', '#54450f', '#45681e', '#4a9152', '#64a8a8', '#85b6ce', '#cde5f9', '#eef3ff'),c(4,8,7,2,6,1,3,5)),
   Sunset = rbind(c('#493f57', '#6c5975', '#ab7173', '#ca8977', '#db9d76', '#e6ad7a', '#f9dfa8'),c(3,4,7,5,1,6,2)),
@@ -39,7 +39,7 @@ pnw_palettes <- list(
 #' of the region, and checked for color blind safety using \href{https://gka.github.io/palettes/#/9|s|00429d,96ffea,ffffe0|ffffe0,ff005e,93003a|1|1}{Chroma.js Color Palette Helper}.
 #' If omitted, all colors are used.
 #' @param type Usage of palette as "continuous" or "discrete". Continuous usage interpolates between colors to create
-#' a scale of values. Operates as continuous if omitted.
+#' a scale of values. If omitted, function assumes continuous if n > length of palette, and discrete if n < length of palette.
 #'
 #' @return
 #' @export
@@ -49,13 +49,9 @@ pnw_palettes <- list(
 #' pnw_palette("Bay",3)
 pnw_palette <- function(name, n, type = c("discrete", "continuous")) {
 
-  if (missing(type)) {
-    type <- "continuous"
-  }
-  type <- match.arg(type)
-
-
   pal <- pnw_palettes[[name]]
+
+
   if (is.null(pal)){
     stop("Palette not found.")
   }
@@ -64,9 +60,17 @@ pnw_palette <- function(name, n, type = c("discrete", "continuous")) {
     n <- length(pal[1,])
   }
 
-  if (type == "discrete" && n > length(pal)) {
+  if (missing(type)) {
+    if(n > length(pal[1,])){type <- "continuous"}
+    else{ type <- "discrete"}
+  }
+  type <- match.arg(type)
+
+
+  if (type == "discrete" && n > length(pal[1,])) {
     stop("Number of requested colors greater than what palette can offer")
   }
+
 
   out <- switch(type,
                 continuous = grDevices::colorRampPalette(pal[1,])(n),
